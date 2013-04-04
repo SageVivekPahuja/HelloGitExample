bca1a2373dfe7a8a64ef5101ca54be5b4598fb97
Vivek Pahuja <vivek.pahuja@sage.com>
2013-04-04 15:11:02 -0400

    Added fortune feature

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -12,7 +12,31 @@ namespace HelloGit
         {
             Console.WriteLine(("Hello Git"));
             Console.WriteLine(("You've been modified."));
+            Console.WriteLine(("Todays fortune:" + GetFortune()));
             Console.ReadKey();
+        }
+
+        private static  string GetFortune()
+        {
+            Random rnd = new Random();
+            int whichOne = rnd.Next(3);
+            string fortune = "";
+            switch (whichOne)
+            {
+                case 1:
+                    fortune = "You will meet a dark stranger";
+                    break;
+                case 2:
+                    fortune = "Your code will be bug-free";
+                    break;
+                case 3:
+                    fortune = "Avoid dog today";
+                    break;
+                default:
+                    fortune= "No fortune for you";
+                    break;
+            }
+            return fortune;
         }
     }
 }
